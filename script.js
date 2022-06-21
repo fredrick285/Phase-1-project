569c0603e6df82d6cab59f629a3830611d6a1ee0
Fredrick Lokidor <106410580+fredrick285@users.noreply.github.com>
2022-06-21 10:19:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let addToCartButtons = document.getElementsByClassName('btn-primary')
+let cartContainer = document.getElementsByTagName('tbody')[0]
+let quantityFields = document.getElementsByClassName('num')
+let delete_buttons = document.getElementsByClassName('uk-button-danger')
+
